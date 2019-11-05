c68f4a3d585f9dcbeeefa7860bc5a71d1c3d2d9f
hongxuli <34845838+hongxuli@users.noreply.github.com>
2019-11-05 00:08:38 -0600

    some changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+LOGIN_REDIRECT_URL = '/blog/'
